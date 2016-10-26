d617b27177b459abdddc2209faa8b37f9ff1f440
Vasyl <admin@gmail.com>
2016-10-26 20:30:33 +0300

    dobavil

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+	<h1>YO-ho-ho</h1>
+	<img src="http://www.extreme-factory.ru/wp-content/uploads/2011/12/Santa-Klausyi.jpg" style="width: 100%"
+    </body>
+</html>
